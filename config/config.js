30aabdc6c728df615b39351be7eb6af2f66ee5f6
执念 <717503359@qq.com>
2018-12-11 11:40:03 +0800

    修改一个数据库连接错误

--- config/config.js
+++ config/config.js
@@ -1,31 +1,31 @@
-const env = require('env2');
+const env2 = require('env2');
 
 if (process.env.NODE_ENV === 'production') {
-  env('./.env.port')
+  env2('./.env.prod');
 } else {
-  env('./.env')
+  env2('./.env');
 }
+
 
 const { env } = process;
 
 module.exports = {
-
-  "development": {
-    "username": env.MYSQL_USERNAME,
-    "password": env.MYSQL_PASSWORD,
-    "database": env.MYSQL_DATABSES,
-    "host": env.MYSQL_HOST,
-    "port": env.MYSQL_PORT,
-    "dialect": "mysql",
-    "operatorsAliases": false
+  development: {
+    username: env.MYSQL_USERNAME,
+    password: env.MYSQL_PASSWORD,
+    database: env.MYSQL_DB_NAME,
+    host: env.MYSQL_HOST,
+    port: env.MYSQL_PORT,
+    dialect: 'mysql',
+    operatorsAliases: false,
   },
-  "production": {
-    "username": env.MYSQL_USERNAME,
-    "password": env.MYSQL_PASSWORD,
-    "database": env.MYSQL_DATABSES,
-    "host": env.MYSQL_HOST,
-    "port": env.MYSQL_PORT,
-    "dialect": "mysql",
-    "operatorsAliases": false
-  }
-}
+  production: {
+    username: env.MYSQL_USERNAME,
+    password: env.MYSQL_PASSWORD,
+    database: env.MYSQL_DB_NAME,
+    host: env.MYSQL_HOST,
+    port: env.MYSQL_PORT,
+    dialect: 'mysql',
+    operatorsAliases: false,
+  },
+};
